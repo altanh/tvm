52a093b322910208730bca17a05ad46778cac9c8
Tristan Konolige <tristan.konolige@gmail.com>
2020-11-05 09:57:07 -0800

    formatting

--- python/tvm/relay/op/strategy/generic.py
+++ python/tvm/relay/op/strategy/generic.py
@@ -1059,16 +1059,19 @@ def schedule_scatter_add(attrs, outs, target):
     with target:
         return topi.generic.schedule_scatter_add(outs)
 
+
 # scatter_nd
 @override_native_generic_func("scatter_nd_strategy")
 def scatter_nd_strategy(attrs, inputs, out_type, target):
     """scatter_nd generic strategy"""
     strategy = _op.OpStrategy()
     strategy.add_implementation(
-        wrap_compute_scatter_nd(topi.scatter_nd), wrap_topi_schedule(topi.generic.schedule_extern),
+        wrap_compute_scatter_nd(topi.scatter_nd),
+        wrap_topi_schedule(topi.generic.schedule_extern),
         name="scatter_nd.generic",
     )
     return strategy
+
 
 def wrap_compute_scatter_nd(topi_compute):
     """Wrap scatter_nd topi compute"""
